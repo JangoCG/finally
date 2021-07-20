29ac911c5c2ec9236691b63855d3f00314791ecd
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-07-20 20:47:55 +0200

    add header and circular progress bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 import StartScreen from './screens/StartScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header title="Get ready to fast"></Header>
       <StartScreen></StartScreen>
     </View>
   );
@@ -13,9 +15,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
 
 const StartScreen = (props) => {
   return (
     <View>
       <Text>Start Screen works</Text>
-      <Text>does vs code work?</Text>
+      <AnimatedCircularProgress
+        size={120}
+        width={15}
+        fill={100}
+        tintColor="#00e0ff"
+        onAnimationComplete={() => console.log("onAnimationComplete")}
+        backgroundColor="#3d5875"
+      />
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({});
 
