e5704c37b869eadf1b57bb68579300d2f84ee467
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-07-20 21:50:15 +0200

    add constants for color and improve progress bar

---
+++ constants/colors.tsx
@@ -0,0 +1,6 @@
+export default {
+    // TODO: Change primary and success
+    primary: "#f7287b",
+    accent: "#c717fc",
+    success: "#3D9970"
+}
--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -1,23 +1,66 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { AnimatedCircularProgress } from "react-native-circular-progress";
+import colors from "../constants/colors";
 
 const StartScreen = (props) => {
   return (
-    <View>
-      <Text>Start Screen works</Text>
+    <View style={styles.screen}>
       <AnimatedCircularProgress
-        size={120}
-        width={15}
+        size={270}
+        width={30}
         fill={100}
-        tintColor="#00e0ff"
+        duration={2000}
+        tintColor="lightgrey"
         onAnimationComplete={() => console.log("onAnimationComplete")}
         backgroundColor="#3d5875"
       />
+       <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title="Start your 16h fast"
+                onPress={() => {}}
+                color={"white"}
+              />
+            </View>
+          </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+      flex: 1,
+      padding: 10,
+      alignItems: "center",
+      justifyContent:"center"
+    },
+    title: {
+      fontSize: 20,
+      marginVertical: "10",
+    },
+    inputContainer: {
+      width: 300,
+      maxWidth: "80%",
+      alignItems: "center",
+    },
+    buttonContainer: {
+    //   flexDirection: "row",
+      width: "100%",
+      justifyContent: "center",
+      alignItems:"center",
+      paddingHorizontal: 15,
+    },
+    button: {
+      marginTop: 40,
+      width: 190,
+      backgroundColor: colors.success,
+      borderRadius: 20,
+    },
+    input: {
+      width: 50,
+      textAlign: "center",
+    },
+  })
 
 export default StartScreen;
