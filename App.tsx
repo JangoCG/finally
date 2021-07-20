61b6da3b4d979652f8542d4d74b7bd5f2de73385
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-07-20 20:31:23 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import StartScreen from './screens/StartScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StartScreen></StartScreen>
     </View>
   );
 }
---
+++ screens/StartScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const StartScreen = (props) => {
+  return (
+    <View>
+      <Text>Start Screen works</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default StartScreen;
