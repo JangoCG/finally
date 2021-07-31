cd73208c99c3f810be15da67a4aee629035c3a37
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-07-31 16:51:19 +0200

    fix header position

--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -1,27 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Text, Button, Easing } from "react-native";
 import { AnimatedCircularProgress } from "react-native-circular-progress";
+import Header from "../components/Header";
 import Navigation from "../components/Navigation";
 import colors from "../constants/colors";
 
 const StartScreen = (props) => {
+  const [fill, setFill] = useState(0);
   let circularProgressRef: AnimatedCircularProgress | null;
   const clickHandler = () => {
     circularProgressRef?.reAnimate(0, 100, 10000);
   };
 
+
+
   return (
     <View style={styles.screen}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>FINALLY</Text>
+        <Text style={styles.headerText}>Hello</Text>
+      </View>
       <View style={styles.contentContainer}>
         <AnimatedCircularProgress
           style={styles.progressBar}
           size={270}
           width={30}
-          fill={100}
+          fill={fill}
           ref={(ref) => (circularProgressRef = ref)}
           tintColor="lightgrey"
           backgroundColor="#3d5875"
-        />
+        >
+        
+          {
+            (fill: number) => (
+              <View>
+              <Text>
+                {fill.toFixed()}
+              </Text>
+             < Text>testing</Text>
+              </View>
+           
+            )
+          }
+          
+          
+        </AnimatedCircularProgress>
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button
@@ -38,6 +61,16 @@ const StartScreen = (props) => {
 };
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "green",
+    textAlign: "center",
+    alignContent: "center",
+    marginTop: 50
+    // marginBottom: 200
+  },
+  headerText: {
+    textAlign: "center"
+  },
   screen: {
     flex: 1,
     padding: 10,
